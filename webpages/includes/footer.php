272930cdd999fc9c742c8513b0f79d3c3832d455
Wesley Teubl <59846142+SimplyWesley@users.noreply.github.com>
2023-10-17 16:31:04 +0200

    Update footer.php

--- webpages/includes/footer.php
+++ webpages/includes/footer.php
@@ -1,8 +1,8 @@
 <footer>
     <div class="footer">
         <p class="footer-text">
-            <a href="mailto:170435@student.horizoncollege.nl">Wesley Teubl</a> |
-            <a href="mailto:170737@student.horizoncollege.nl">Justin Kroeze</a> |
+            <a href="wesleyteubl.com" target="_blank">Wesley Teubl</a> |
+            <a href="justinkroeze.com" target="_blank">Justin Kroeze</a> |
             <a href="mailto:169063@student.horizoncollege.nl">Jennifer Bot</a> |
             <a href="mailto:140649@student.horizoncollege.nl">Mitch Andria Valero</a>
         </p>
